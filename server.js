36b7e6d2b67bc5c3c489a1471e6357b56c2cf362
georgekara13 <george_kara_13@hotmail.com>
2020-04-01 00:31:39 +0300

    Add user auth, user routes

--- server.js
+++ server.js
@@ -45,6 +45,31 @@ app.get('/api/getbooks', (req, res) => {
   })
 })
 
+app.get('/api/getreviewerbyid', (req,res) => {
+  let id = req.query.id
+
+  User.findById(id, (err, doc) => {
+    if(err) return res.status(400).send(err)
+    res.json({
+      name: doc.name,
+      lastname: doc.lastName
+    })
+  })
+})
+
+app.get('/api/getusers', (req, res) => {
+  User.find({},(err, users) => {
+    if(err) return res.status(400).send(err)
+
+    //filter some info - we dont want to expose passwords, roles
+    const userRes = users.map((user) => {
+      return {_id: user._id, email: user.email, name: user.name, lastName: user.lastName}
+    })
+
+    res.status(200).send(userRes)
+  })
+})
+
 //POST
 app.post('/api/addbook', (req,res) => {
   const book = new Book(req.body)
@@ -54,6 +79,40 @@ app.post('/api/addbook', (req,res) => {
     res.status(200).json({
       post: true,
       bookId: doc._id
+    })
+  })
+})
+
+app.post('/api/register',(req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({success: false})
+    res.status(200).json({
+      success: true,
+      user: doc
+    })
+  })
+})
+
+app.post('/api/login', (req, res) => {
+  User.findOne({'email': req.body.email}, (err, user) => {
+    if(!user) return res.json({isAuth: false, message: 'Auth failed, email not found'})
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({
+        isAuth: false,
+        message: 'Wrong password'
+      })
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+        //send user data as a response cookie
+        res.cookie('auth', user.token).json({
+          isAuth: true,
+          id: user._id,
+          email: user.email
+        })
+      })
     })
   })
 })
